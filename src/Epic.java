855578aed9cedf0bf5bd3716855b59f44c291c54
Aleksey Kim <lesha.kim.2912@gmail.com>
2025-07-02 23:57:29 +0500

    УпСоздание менеджера хзадач гÐде приÑ

---
+++ src/Epic.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Epic extends Task {
+    private final List<Integer> subtaskIds;
+
+    public Epic(int id, String name, String description) {
+        super(id, name, description, Status.NEW);
+        this.subtaskIds = new ArrayList<>();
+    }
+
+    public List<Integer> getSubtaskIds() {
+        return new ArrayList<>(subtaskIds);
+    }
+
+    public void addSubtaskId(int subtaskId) {
+        subtaskIds.add(subtaskId);
+    }
+
+    public void removeSubtaskId(int subtaskId) {
+        subtaskIds.remove(Integer.valueOf(subtaskId));
+    }
+
+    @Override
+    public String toString() {
+        return "Epic{" +
+                "id=" + getId() +
+                ", name='" + getName() + '\'' +
+                ", description='" + getDescription() + '\'' +
+                ", status=" + getStatus() +
+                ", subtaskIds=" + subtaskIds +
+                '}';
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,41 @@
 public class Main {
-
     public static void main(String[] args) {
-        System.out.println("Поехали!");
+        TaskManager manager = new TaskManager();
+
+
+        Task task1 = manager.createTask(new Task(0, "task 1", "description 1", Status.NEW));
+        Task task2 = manager.createTask(new Task(0, "tas 2", "description 2", Status.IN_PROGRESS));
+
+        Epic epic1 = manager.createEpic(new Epic(0, "Epic 1", "description Epic 1"));
+        Subtask subtask1 = manager.createSubtask(new Subtask(0, "task 1", "description subtask 1", Status.NEW, epic1.getId()));
+        Subtask subtask2 = manager.createSubtask(new Subtask(0, "task 2", "description subtask 2", Status.IN_PROGRESS, epic1.getId()));
+
+        Epic epic2 = manager.createEpic(new Epic(0, "Epic 2", "description Epic 2"));
+        Subtask subtask3 = manager.createSubtask(new Subtask(0, "subtask 3", "description subtask 3", Status.DONE, epic2.getId()));
+
+        System.out.println("all tasks: " + manager.getAllTasks());
+        System.out.println("all Epics: " + manager.getAllEpics());
+        System.out.println("all subtasks: " + manager.getAllSubtasks());
+
+        task1.setStatus(Status.DONE);
+        manager.updateTask(task1);
+
+        subtask1.setStatus(Status.DONE);
+        manager.updateSubtask(subtask1);
+
+        subtask2.setStatus(Status.DONE);
+        manager.updateSubtask(subtask2);
+
+        System.out.println("all tasks: " + manager.getAllTasks());
+        System.out.println("all Epics: " + manager.getAllEpics());
+        System.out.println("all subtasks: " + manager.getAllSubtasks());
+
+
+        manager.deleteTaskById(task1.getId());
+        manager.deleteEpicById(epic2.getId());
+
+        System.out.println("all tasks: " + manager.getAllTasks());
+        System.out.println("all Epics: " + manager.getAllEpics());
+        System.out.println("all subtasks: " + manager.getAllSubtasks());
     }
 }
